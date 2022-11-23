f5af75c048dc861cacd3d25d907091995c5c86e2
latant <latinovitsantal@gmail.com>
2022-11-23 15:57:56 +0100

    configure monorepo

---
+++ packages/togm/jest.config.js
@@ -0,0 +1,27 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  passWithNoTests: true,
+  roots: ["<rootDir>/src"],
+  testMatch: ["<rootDir>/src/**/*.(test|spec).(ts|js)"],
+  coverageDirectory: "coverage",
+  moduleFileExtensions: ["js", "json", "ts", "node"],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testPathIgnorePatterns: ["/node_modules/", "/lib/", "/dist/"],
+  modulePathIgnorePatterns: ["dist"],
+  displayName: {
+    color: "blue",
+    name: "togm",
+  },
+  coverageReporters: ["text", "json", "html", "cobertura", "lcov"],
+  collectCoverageFrom: ["src/**/*.{ts,tsx}", "!**/node_modules/**", "!**/vendor/**"],
+  testTimeout: 10000,
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+};
